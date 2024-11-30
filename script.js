bd64eba29bb6b0f3a7e6fb037f90ef2a0563c1a4
Lee Wei Hang <167171171+radrium@users.noreply.github.com>
2024-11-30 11:26:29 +0800

    added validation for feedback form so that all must be field and a function where when user hover over a food using mouse will be shown an image of how the food will look like

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
-document.querySelector('.feedback-form').addEventListener('submit', function (e) {
-    e.preventDefault();
+if (window.location.pathname.includes('contact.html')) {
+    document.querySelector('.feedback-form').addEventListener('submit', function (e) {
+        e.preventDefault();
 
 
-    const name = document.getElementById('name').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const feedbackType = document.getElementById('feedback-type').value;
-    const message = document.getElementById('message').value.trim();
+        var name = document.getElementById('name').value.trim();
+        var email = document.getElementById('email').value.trim();
+        var message = document.getElementById('message').value.trim();
 
-    if (name === '' || email === '' || message === '') {
-        alert('Please fill in all fields');
-        return;
-    }
+        if (name === '' || email === '' || message === '') {
+            alert('Please fill in all fields');
+            return;
+        }
 
-    alert('Thank you for your feedback!');
-});
+        alert('Thank you for your feedback!');
+    });
+}
+if (window.location.pathname.includes('menu.html')) 
+{
+    var backToTopButton = document.getElementById("backToTop");
+    backToTopButton.addEventListener("click", function()
+    {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+
+    var text = document.querySelectorAll('.hover-text');
+    var images = document.querySelectorAll('.hidden-image');
+
+    text.forEach(function(text, index) {
+        var image = images[index];
+        text.addEventListener('mouseenter', function() {
+            image.style.opacity = 1;
+            image.style.display = 'inline';
+        });
+        text.addEventListener('mouseleave', function() {
+            image.style.opacity = 0;
+            setTimeout(function() 
+            {
+                image.style.display = 'none';
+            }, 200);
+        });
+    });
+}
